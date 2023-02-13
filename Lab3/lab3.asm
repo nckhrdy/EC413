7b108872a86533b1d5916a9a1d71cffec9cf1458
nckhrdy <nckhrdy@bu.edu>
2023-02-13 14:29:32 -0500

    Update lab3.asm

--- Lab3/lab3.asm
+++ Lab3/lab3.asm
@@ -48,9 +48,30 @@ main:
 #Code for Item 2
 #Count number of occurrences of letter "f" in Text string
 
+    li $t0, 0             #initializing the for loop variable i = 0
+    li $t1, 0             #counter variable 
+    lw $t2, Hello_len     #load the size of the word hello
+    la $t3, Hello         #load address of Hello
+    li $t4, 'f'           #character ascii we are counting
 
+loop1: 
+    bge $t0,$t2,endLoop1  #if greater or equal end the loop and jump to label
+    add $t3,$t3,1         #else add 1 to the address spot
+    lb $t5,0($t3)         #load value character of hello into register t5
+    beq $t4,$t5,count     #if t5 is equal to 'f' then jump to count
+    add $t0,$t0,1         #else increment i + 1
+    j loop1               #restart loop
 
+count: 
+  add $t1,$t1,1           #add one to the count variable
+  add $t0,$t0,1           #add one to i
+  j loop1                 #jump back to loop1
 
+endLoop1:              #print statements
+  xor $a0,$a0,$a0       #set contents of a0 to zero
+  add $a0,$a0,$t1      #add the value of the count variable to reg a0
+  li $v0,1              #tell the machine to run funciton 
+  syscall             #system call to print to console i/o
 
 
 
@@ -67,7 +88,28 @@ main:
 # Print the integer value of numbers from 8 and less than AnInt
 #
 
+  li	$t0, 0		#initializing counter i
+  lw	$t1, AnInt	#load the whole word for AnInt
+	xor	$a0, $a0, $a0  #sets contents of a0 to 0
+	
 
+Loop2:
+	bge	$t0, $t1, endLoop2
+	add	$a0, $a0, $t0		# Printing the int
+	li	$v0, 1
+	syscall
+	xor	$a0, $a0, $a0
+	
+	la	$a0, space
+	li	$v0, 4
+	syscall
+	xor	$a0, $a0, $a0
+	
+	add	$t0, $t0, 1
+	j	Loop2	
+
+endLoop2:	
+	xor	$a0, $a0, $a0
 
 
 
