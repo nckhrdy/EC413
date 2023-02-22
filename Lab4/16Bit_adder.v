9ce27f308b57795c18af9431bca4c8e6f2cefd16
Nicholas Hardy <91441308+nckhrdy@users.noreply.github.com>
2023-02-22 12:16:17 -0500

    Update 16Bit_adder.v

--- Lab4/16Bit_adder.v
+++ Lab4/16Bit_adder.v
@@ -1,1 +1,20 @@
-
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/21/2023 02:05:02 PM
+// Design Name: 
+// Module Name: four_bitadder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
