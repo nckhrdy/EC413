ccc149b7d67570106a7ea8bdfd2bbd75a71ce408
Nicholas Hardy <91441308+nckhrdy@users.noreply.github.com>
2023-03-13 19:06:37 -0400

    Update Verification_RegAdder.v

--- Lab5/Verification_RegAdder.v
+++ Lab5/Verification_RegAdder.v
@@ -1,1 +1,20 @@
-
+module Verification_RegAdder(c_out,a,b,c_in,select,clk);
+  parameter n=8; 
+  input [n-1:0] a; 
+  input [n-1:0] b; 
+  input c_in; 
+  output reg c_out;
+  output reg [n-1:0] sum; 
+  output select; 
+  input clk;
+  
+  wire [n-1:0] sum_temp; 
+  wire c_out_temp;
+  assign {c_out_temp, sum_temp} = a + b + c_in;
+  
+  always@(posedge clk)
+    begin 
+      c_out <= c_out_temp; 
+      sum <= (select) ? ~a : sum_temp;
+    end
+endmodule
